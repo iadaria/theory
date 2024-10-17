50f1aaccc232a55fb39ba3c4dc4dd9d02d6c56c4
iadaria <jadarya@mail.ru>
2024-10-17 17:46:55 +0400

    add Clock

--- ru/react/examples/src/base/Element/Element.tsx
+++ ru/react/examples/src/base/Element/Element.tsx
@@ -1,8 +1,11 @@
 import { ShowTickButton } from "./ex0_4_Tick";
+import { Clock } from "./ex0_5_Tick";
 
 export function Element() {
 
     let example = <ShowTickButton />
+
+    example = <Clock />
 
     return example;
 }
--- ru/react/examples/src/base/Element/ex0_5_Tick.tsx
+++ ru/react/examples/src/base/Element/ex0_5_Tick.tsx
@@ -1,9 +1,22 @@
-export function Clock(props: {date: Date }) {
+import { useEffect, useState } from "react";
+
+export function Clock() {
+    const [date, setDate] = useState(new Date());
+   
+    function tick() {
+        setDate(new Date());
+    }
+
+    useEffect(() => {
+        let timerId = setInterval(tick, 1000);
+        return () => clearInterval(timerId);
+    }, []);
+
     return (
         <div>
             <h1>Привет, мир!</h1>
             <h2>
-                Сейчас {props.date.toLocaleTimeString()}.
+                Сейчас {date.toLocaleTimeString()}.
             </h2>
         </div>
     );
